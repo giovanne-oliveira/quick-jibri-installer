9c818c82a60fd2459ea03088e44bc00e91bf3cdc
Giovanne Oliveira <giovanne@giovanne.dev>
2022-11-16 15:01:47 +0000

    tweaks on finalize_recording script

--- add-jibri-node-no-interaction.sh
+++ add-jibri-node-no-interaction.sh
@@ -321,19 +321,15 @@ cat << REC_DIR > "$REC_DIR"
 
 RECORDINGS_DIR="$DIR_RECORD"
 
-echo "This is a dummy finalize script" > /tmp/finalize.out
-echo "The script was invoked with recordings directory $RECORDINGS_DIR." >> /tmp/finalize.out
-echo "You should put any finalize logic (renaming, uploading to a service" >> /tmp/finalize.out
-echo "or storage provider, etc.) in this script" >> /tmp/finalize.out
-
 chmod -R 775 \$RECORDINGS_DIR
 
 #Rename folder.
 LJF_PATH="\$(find \$RECORDINGS_DIR -exec stat --printf="%Y\t%n\n" {} \; | sort -n -r|awk '{print\$2}'| grep -v "meta\|-" | head -n1)"
 NJF_NAME="\$(find \$LJF_PATH |grep -e "-"|sed "s|\$LJF_PATH/||"|cut -d "." -f1)"
 NJF_PATH="\$RECORDINGS_DIR/\$NJF_NAME"
+mv $LJF_PATH $NJF_PATH
 
-php /home/jibri/uploader/upload.php $NJF_PATH
+php /home/jibri/uploader/index.php $NJF_PATH
 
 exit 0
 REC_DIR
